84d6f8b48716c1d46486b2369bbfcdc78d526c9c
GOKULAKRISHNAN K <230701094@rajalakshmi.edu.in>
2025-04-22 03:52:32 -0700

    Delete backend/rooms directory

--- backend/rooms/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Room
-
-@admin.register(Room)
-class RoomAdmin(admin.ModelAdmin):
-    list_display = ('room_number', 'block', 'department', 'type', 'min_capacity', 'max_capacity', 'has_projector', 'has_ac')
-    search_fields = ('room_number', 'block')
-    list_filter = ('department', 'type', 'has_projector', 'has_ac')
-    ordering = ('block', 'room_number')
